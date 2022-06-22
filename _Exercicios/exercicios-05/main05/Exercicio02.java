9182cbcf9aa014886088abf18d192e206e380373
Victor Toledo <victorptoledo@outlook.com>
2022-06-22 13:03:08 -0300

    Adição dos exercícios

---
+++ _Exercicios/exercicios-05/main05/Exercicio02.java
@@ -0,0 +1,21 @@
+package main05;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N % 2 == 0) {
+            System.out.println("PAR");
+        } else {
+            System.out.println("IMPAR");
+        }
+
+        sc.close();
+    }
+}
---
+++ _Exercicios/exercicios-06/main06/Exercicio08.java
@@ -0,0 +1,22 @@
+package main06;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int fat = 1;
+        for (int i = 1; i <= n; i++) {
+            fat = fat * i;
+        }
+
+        System.out.println(fat);
+
+        sc.close();
+    }
+}
