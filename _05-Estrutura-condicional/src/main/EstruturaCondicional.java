768159d5c5cd2c9cb690ba2092ce80a711fc7c72
Victor Toledo <victorptoledo@outlook.com>
2022-06-22 13:03:31 -0300

    Updates

--- _05-Estrutura-condicional/src/main/EstruturaCondicional.java
+++ _05-Estrutura-condicional/src/main/EstruturaCondicional.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class EstruturaCondicional {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		int hora;
+        int hora;
 
-		System.out.println("Que horas são?");
-		hora = scanner.nextInt();
+        System.out.println("Que horas são?");
+        hora = scanner.nextInt();
 
-		if (hora < 12) {
-			System.out.println("Bom dia!");
-		} else if (hora >= 12 && hora <= 18) {
-			System.out.println("Boa tarde!");
-		} else {
-			System.out.println("Boa noite!");
-		}
+        if (hora < 12) {
+            System.out.println("Bom dia!");
+        } else if (hora <= 18) {
+            System.out.println("Boa tarde!");
+        } else {
+            System.out.println("Boa noite!");
+        }
 
-		scanner.close();
+        scanner.close();
 
-	}
+    }
 
 }
