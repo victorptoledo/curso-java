43cc096b37d329d8f96942a9153b913083229197
Victor Toledo <victorptoledo@outlook.com>
2022-06-30 19:36:04 -0300

    Exercicío resolvido
    Signed-off-by: Victor Toledo <victorptoledo@outlook.com>

---
+++ _Exercicios/exercicios-13/entity13/Comment.java
@@ -0,0 +1,21 @@
+package entity13;
+
+public class Comment {
+
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
