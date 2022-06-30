49c3f70f1e22c7a0f6b1be70f827cf53a985a292
Victor Toledo <victorptoledo@outlook.com>
2022-06-29 21:28:01 -0300

    Introdução a Enumerações e composições
    Signed-off-by: Victor Toledo <victorptoledo@outlook.com>

---
+++ _Exercicios/exercicios-13/entity13/Department.java
@@ -0,0 +1,21 @@
+package entity13;
+
+public class Department {
+
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ _Exercicios/exercicios-13/enums13/WorkerLevel.java
@@ -0,0 +1,8 @@
+package enums13;
+
+public enum WorkerLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR
+}
